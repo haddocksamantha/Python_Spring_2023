d4e09eb74c1200e3f43cb6881a4bb94feb7e1395
Samantha Haddock <59586234+haddocksamantha@users.noreply.github.com>
2023-01-25 21:54:38 -0700

    functions

---
+++ samantha_haddock_inclass2.py
@@ -0,0 +1,54 @@
+
+
+"""
+Author: Samantha Haddock
+Course: CS_1400
+Date:1_25_2023
+
+The purpose of this code is to be able to use it as a calculator that adds,
+subtracts, multiplies, and divides positive integers using functions. 
+It also uses declared variables as well.
+
+"""
+
+#variables
+num1 = int(input(5))
+num2 = int(input(8))
+
+
+#here is the main function
+
+def ___main___(result):
+    print(result)
+
+
+#here are the other functions
+
+def addition(add1,add2):
+    sum = add1 + add2
+    ___main___(sum)
+
+def subtraction(sub1,sub2):
+    difference = sub1 - sub2
+    ___main___(difference)
+
+def division(div1,div2):
+    remainder = div1 % div2
+    ___main___(remainder)
+
+def multiplication(mul1,mul2):
+    product = mul1 * mul2
+    ___main___(product)
+
+
+#here is the calculator
+
+addition(num1,num2)
+subtraction(num1,num2)
+division(num1,num2)
+multiplication(num1,num2)
+
+
+
+
+
